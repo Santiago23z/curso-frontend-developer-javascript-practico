579fad4247e81d3d0630b0fa2f1a40f6ebaa2817
Santiago23z <santizapataalvares@gmail.com>
2022-08-30 06:34:47 -0500

    Updates

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const email = document.querySelector('.navbar-email');
-const desktop = document.querySelector('.desktop-menu')
+const desktop = document.querySelector('.desktop-menu');
+const menuHam = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
+menuHam.addEventListener('click', toggleMobile)
 email.addEventListener('click', toggle);
 
 function toggle() {
-    desktop.classList.toggle('inactive')
+    desktop.classList.toggle('inactive');
+}
+
+function toggleMobile() {
+    menuMobile.classList.toggle('inactive');
 }
